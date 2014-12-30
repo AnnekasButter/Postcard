37aa7547d9b96546f155df128f7a3296c858bc07
Araneetae Guy <spacreations@Araneetaes-MBP.attlocal.net>
2014-12-29 23:32:47 -0500

    nameLabel functionality

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -14,8 +14,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
+        // First Challenge Add a nameLabel set to hidden, update nameLabel's text to display info in nameTextField, Change nameLabel's textColor to blue, Clear nameTextField's text
+    @IBOutlet weak var nameLabel: UILabel!
+   
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -32,8 +34,17 @@ class ViewController: UIViewController {
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
         
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
+        
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
+        
+
         
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
         
